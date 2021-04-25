263eef85705a58b63e63711c0ae1f988af49e28d
prasanthgajula08 <prasanth080898@gmail.com>
2021-04-24 20:24:18 -0400

    Implement dummy webscrap

--- Twitter_Review/WebScraper/views.py
+++ Twitter_Review/WebScraper/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import bs4
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def fetchData(request):
+    searchedValue = request.POST['searchValue']
+    url = "https://www.google.com/search?q=" + searchedValue
+    html_code = requests.get(url)
+    bs_code = bs4.BeautifulSoup(html_code.text, 'lxml')
+    title = bs_code.select('title')[0].getText()
+    return render(request, 'info.html', {'title':title})
