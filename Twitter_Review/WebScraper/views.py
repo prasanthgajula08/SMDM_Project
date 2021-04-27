056e633dd74c845818a3a431f07de8235a94883d
prasanthgajula08 <prasanth080898@gmail.com>
2021-04-27 17:58:29 -0400

    Add poster feature (need fix)

--- Twitter_Review/WebScraper/views.py
+++ Twitter_Review/WebScraper/views.py
@@ -9,8 +9,13 @@ def home(request):
 
 def fetchData(request):
     searchedValue = request.POST['searchValue']
-    url = "https://www.google.com/search?q=" + searchedValue
+    #url = "https://www.google.com/search?q=" + searchedValue
+    url = "https://www.reelgood.com/movie/" + searchedValue
     html_code = requests.get(url)
-    bs_code = bs4.BeautifulSoup(html_code.text, 'lxml')
+    bs_code = bs4.BeautifulSoup(html_code.text)
     title = bs_code.select('title')[0].getText()
-    return render(request, 'info.html', {'title':title})
+
+    images = bs_code.find_all("img", {"class": "css-1sz776d e1181ybh1"})
+    image = images[2]['src']
+    
+    return render(request, 'info.html', {'title':title}, {'image':image})
